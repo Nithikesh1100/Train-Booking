c3872c2068c0de2d96f94484c7802202cc7c0b60
NITHIKESH <nithikesh.b2019@vitstudent.ac.in>
2024-12-30 23:08:36 +0530

    added 2

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Navbar from '@/components/ui/Navbar';
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-4">
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="max-w-md w-full space-y-8">
         <div className="text-center">
           <h1 className="text-4xl font-bold text-gray-900 mb-2">
